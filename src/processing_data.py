b7c61c0bb2a4fb1fc948defffd110d1a0145add5
tew <demohandsome@gmail.com>
2024-11-19 22:28:03 +0700

    5th commit

--- src/processing_data.py
+++ src/processing_data.py
@@ -4,64 +4,31 @@ import plotly.express as px
 import plotly.graph_objs as go
 import seaborn as sns
 import missingno as msno
+import pandas as pd
 
-
-def proc(data):
-    #print(data)
-
-    #Overview 'bout Data
-
-    #data.info(verbose=True) #how many column, line/column
-    #print(data.nunique()) #differant value on each column
-    #print(data.describe()) #absolutly for describe data
-
-    """
-    # Count null-value, null-ratio
+def overview(data):
+    print(data.info())
+    print(data.nunique())
+    print(data.describe())
+    #----------------------
     null_info = pd.DataFrame({'Null count': data.isnull().sum(), 'Null ratio': data.isnull().sum()/len(data)})
     null_info.drop('Species', inplace=True)
     null_info.sort_values(by='Null ratio', ascending=False, inplace=True)
     print(null_info)
-    """
-
-    #"""
-    # represent null throught image
-    msno.matrix(data)
+    #----------------------
+    fig, ax = plt.subplots(figsize=(9,4))
+    msno.matrix(data, ax=ax)
     plt.show()
-    #"""
-    
-    """
-    # Flowers ratio
-    print('% Setosa:', format((data.Species=='Iris-setosa').sum()/len(data)*100, '.2f'))
-    print('% Versicolor:', format((data.Species=='Iris-versicolor').sum()/len(data)*100, '.2f'))
-    print('% Virginica:', format((data.Species=='Iris-virginica').sum()/len(data)*100, '.2f'))
-    """
-    
-    #Represent data and change data throught data.copy()
-    
+    #----------------------
+    print('Setosa: ', format((data.Species=='Iris-setosa').sum()/len(data)*100, '.2f'))
+    print('Versicolor: ', format((data.Species=='Iris-versicolor').sum()/len(data)*100, '.2f'))
+    print('Virginica: ', format((data.Species=='Iris-virginica').sum()/len(data)*100, '.2f'))
+
+def visualization(data):
     data1 = data.copy()
     
-    #SepalLength Begin
-    
-    #print(data1['SepalLengthCm'].value_counts(dropna=False)) #dropna = Drop Nan: Loai bo non-value, dropna=False => khong loai bo non-value; defaul: dropna=True
-    
-    plt.figure(figsize=(9,4))
-    #1st subplot
-    plt.subplot(1,2,1)
-    sns.histplot(data1.SepalLengthCm, bins=20, kde=True)
-    plt.title('Histplot diagram for SepalLengthCm')
-    
-    #2nd subplot
-    plt.subplot(1,2,2)
-    sns.boxplot(x="Species", y="SepalLengthCm", data=data1)
-    sns.stripplot(x='Species', y='SepalLengthCm', data=data1, jitter=True, edgecolor='grey')
-    plt.title('Boxplot for SepalLengthCm')
-    
-    plt.show()
-    #SepalLengh End
-    
-    #SepalWidth Begin
-    
-    plt.figure(figsize=(9,4))
-    
-    plt.show()
-    #SepalWidth End
+    print(data1['SepalLengthCm'].value_counts(dropna=False))
+
+def proc(data):
+    #overview(data)
+    visualization(data)
