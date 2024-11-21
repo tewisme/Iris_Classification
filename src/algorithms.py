ab52824d5c5d93bd07ff577a8c928ec9e1e44f43
tew <demohandsome@gmail.com>
2024-11-21 19:20:36 +0700

    9th commit

--- src/algorithms.py
+++ src/algorithms.py
@@ -6,7 +6,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import recall_score, f1_score
 #model selection
 from sklearn.model_selection import KFold, train_test_split, cross_val_score
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier     
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn import linear_model
 from sklearn.tree import DecisionTreeClassifier
@@ -20,6 +20,63 @@ from sklearn.preprocessing import LabelEncoder
 def Naive_Bayes(x_train, x_test, y_train, y_test):
     gaussian = GaussianNB()
     gaussian.fit(x_train, y_train)
+    y_pred = gaussian.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    accuracy_nb = round(accuracy*100, 2)
+    acc_gaussian = round(gaussian.score(x_train, y_train)*100, 2)
+    print(accuracy, accuracy_nb)
+    cm = confusion_matrix(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='micro')
+    recall = recall_score(y_test, y_pred, average='micro')
+    f1 = f1_score(y_test, y_pred, average='micro')
+
+def Dec_Tree(x_train, x_test, y_train, y_test):
+    dec_tree = DecisionTreeClassifier()
+    dec_tree.fit(x_train, y_train)
+    y_pred = dec_tree.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    accuracy_dt = round(accuracy*100, 2)
+    print(accuracy, accuracy_dt)
+    cm = confusion_matrix(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='micro')
+    recall = recall_score(y_test, y_pred, average='micro')
+    f1 = f1_score(y_test, y_pred, average='micro')
+
+def Random_Forest(x_train, x_test, y_train, y_test):
+    rd_forest = RandomForestClassifier(n_estimators=100)
+    rd_forest.fit(x_train, y_train)
+    y_pred = rd_forest.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    accuracy_rf = round(accuracy*100, 2)
+    print(accuracy, accuracy_rf)
+    cm = confusion_matrix(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='micro')
+    racall = recall_score(y_test, y_pred, average='micro')
+    f1 = f1_score(y_test, y_pred, average='micro')
+
+def KNN(x_train, x_test, y_train, y_test):
+    knn = KNeighborsClassifier(n_neighbors=3)
+    knn.fit(x_train, y_train)
+    y_pred = knn.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    accuracy_knn = round(accuracy*100, 2)
+    print(accuracy, accuracy_knn)
+    cm = confusion_matrix(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='micro')
+    recall = recall_score(y_test, y_pred, average='micro')
+    f1 = f1_score(y_test, y_pred, average='micro')
+
+def MLP(x_train, x_test, y_train, y_test):
+    mlp = MLPClassifier(random_state=42, max_iter=1000)
+    mlp.fit(x_train, y_train)
+    y_pred = mlp.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    accuracy_mlp = round(accuracy*100, 2)
+    print(accuracy, accuracy_mlp)
+    cm = confusion_matrix(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='micro')
+    recall = recall_score(y_test, y_pred, average='micro')
+    f1 = f1_score(y_test, y_pred, average='micro')
 
 
 def proc(data):
@@ -28,5 +85,9 @@ def proc(data):
     y = data.iloc[:, 4].values
     le = LabelEncoder()
     y = le.fit_transform(y)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
-    Naive_Bayes(x_train, x_test, y_train, y_test)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+    #Naive_Bayes(x_train, x_test, y_train, y_test)
+    #Dec_Tree(x_train, x_test, y_train, y_test)
+    #Random_Forest(x_train, x_test, y_train, y_test)
+    #KNN(x_train, x_test, y_train, y_test)
+    #MLP(x_train, x_test, y_train, y_test)
--- src/processing_data.py
+++ src/processing_data.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import missingno as msno
 import pandas as pd
 
+
 def overview(data):
     print(data.info())
     print(data.nunique())
@@ -23,6 +24,7 @@ def overview(data):
     print('Setosa: ', format((data.Species=='Iris-setosa').sum()/len(data)*100, '.2f'))
     print('Versicolor: ', format((data.Species=='Iris-versicolor').sum()/len(data)*100, '.2f'))
     print('Virginica: ', format((data.Species=='Iris-virginica').sum()/len(data)*100, '.2f'))
+
 
 def visualization(data):
     data1 = data.copy()
@@ -89,45 +91,47 @@ def visualization(data):
 
     plt.show()
 
+
 def relationship(data):
     data1 = data.copy()
     
-    sns.jointplot(x="SepalLengthCm", y="SepalWidthCm", data=data1, size=5, hue="Species")
-    plt.title("Relationship between SepalLengthCm and SepalWidthCm")
+    #sns.jointplot(x="SepalLengthCm", y="SepalWidthCm", data=data1, size=5, hue="Species")
+    #plt.title("Relationship between SepalLengthCm and SepalWidthCm")
     #------------------
-    sns.set_style('whitegrid')
-    sns.lmplot(x='SepalLengthCm', y='SepalWidthCm', data=data1, hue='Species', markers=['o', 'v', 'x'])
-    plt.title("Regression between SepalLengthCm and SepalWidthCm")
+    #sns.set_style('whitegrid')
+    #sns.lmplot(x='SepalLengthCm', y='SepalWidthCm', data=data1, hue='Species', markers=['o', 'v', 'x'])
+    #plt.title("Regression between SepalLengthCm and SepalWidthCm")
     #------------------
-    sns.jointplot(x="PetalLengthCm", y="PetalWidthCm", data=data1, size=5, hue="Species")
-    plt.title("Relationship between PetalLengthCm and PetalWidthCm")
+    #sns.jointplot(x="PetalLengthCm", y="PetalWidthCm", data=data1, size=5, hue="Species")
+    #plt.title("Relationship between PetalLengthCm and PetalWidthCm")
     #------------------
-    sns.set_style('whitegrid')
-    sns.lmplot(x='PetalLengthCm', y='PetalWidthCm', data=data1, hue="Species", markers=['o', 'v', 'x'])
-    plt.title("Regression between PetalLengthCm and PetalWidthCm")
+    #sns.set_style('whitegrid')
+    #sns.lmplot(x='PetalLengthCm', y='PetalWidthCm', data=data1, hue="Species", markers=['o', 'v', 'x'])
+    #plt.title("Regression between PetalLengthCm and PetalWidthCm")
 
-    plt.figure(figsize=(12,6)) #palette must beside hue
-    plt.subplot(2,2,1); sns.barplot(x='Species', y='SepalLengthCm', data=data1, palette='cubehelix', hue='Species')
-    plt.subplot(2,2,2); sns.barplot(x='Species', y='SepalWidthCm', data=data1, palette='Oranges', hue='Species')
-    plt.subplot(2,2,3); sns.barplot(x='Species', y='PetalLengthCm', data=data1, palette='Oranges', hue='Species')
-    plt.subplot(2,2,4); sns.barplot(x='Species', y='PetalLengthCm', data=data1, palette='cubehelix', hue='Species')
+    #plt.figure(figsize=(12,6)) #palette must beside hue
+    #plt.subplot(2,2,1); sns.barplot(x='Species', y='SepalLengthCm', data=data1, palette='cubehelix', hue='Species')
+    #plt.subplot(2,2,2); sns.barplot(x='Species', y='SepalWidthCm', data=data1, palette='Oranges', hue='Species')
+    #plt.subplot(2,2,3); sns.barplot(x='Species', y='PetalLengthCm', data=data1, palette='Oranges', hue='Species')
+    #plt.subplot(2,2,4); sns.barplot(x='Species', y='PetalLengthCm', data=data1, palette='cubehelix', hue='Species')
     
-    plt.figure(figsize=(12,6))
-    sns.heatmap(data1.drop('Species', axis=1).corr(), annot=True, cmap='Dark2_r', linewidths=2)
+    #plt.figure(figsize=(12,6))
+    #sns.heatmap(data1.drop('Species', axis=1).corr(), annot=True, cmap='Dark2_r', linewidths=2)
 
     plt.show()
+
 
 def summarize(data):
     data1 = data.copy()
 
     #sns.pairplot(data=data1, kind='scatter')
     sns.pairplot(data=data1, hue='Species')
-    
+
     plt.show()
 
 
 def proc(data):
     #overview(data)
     #visualization(data)
-    #relationship(data)
-    summarize(data)
+    relationship(data)
+    #summarize(data)
