9584344ef9b0ae8cb3975eb05825aae17ac30c53
tew <demohandsome@gmail.com>
2024-11-21 20:36:20 +0700

    10th commit

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,4 +1,5 @@
 #lib
+import pandas as pd
 #metrics
 from sklearn.metrics import make_scorer, accuracy_score, precision_score
 from sklearn.metrics import classification_report
@@ -17,14 +18,25 @@ from sklearn.neural_network import MLPClassifier
 #model support
 from sklearn.preprocessing import LabelEncoder
 
+results = pd.DataFrame({
+    "Model": [ 'Naive Bayes',
+              'Decision Tree',
+              'Random Forest',
+              'KNN',
+              'Multi-Layer Perceptron',
+              'SVC'],
+    })
+
 def Naive_Bayes(x_train, x_test, y_train, y_test):
     gaussian = GaussianNB()
     gaussian.fit(x_train, y_train)
     y_pred = gaussian.predict(x_test)
     accuracy = accuracy_score(y_test, y_pred)
     accuracy_nb = round(accuracy*100, 2)
-    acc_gaussian = round(gaussian.score(x_train, y_train)*100, 2)
-    print(accuracy, accuracy_nb)
+    acc_nb = round(gaussian.score(x_train, y_train)*100, 2)
+    results.loc[0, 'Score'] = acc_nb
+    results.loc[0, 'Accuracy_Score'] = accuracy_nb
+    #print("Naive Bayes: ", accuracy, ',', accuracy_nb)
     cm = confusion_matrix(y_test, y_pred)
     precision = precision_score(y_test, y_pred, average='micro')
     recall = recall_score(y_test, y_pred, average='micro')
@@ -36,7 +48,10 @@ def Dec_Tree(x_train, x_test, y_train, y_test):
     y_pred = dec_tree.predict(x_test)
     accuracy = accuracy_score(y_test, y_pred)
     accuracy_dt = round(accuracy*100, 2)
-    print(accuracy, accuracy_dt)
+    acc_dt = round(dec_tree.score(x_train, y_train)*100, 2)
+    results.loc[1, 'Score'] = acc_dt
+    results.loc[1, 'Accuracy_Score'] = accuracy_dt
+    #print("Decision Tree: ", accuracy, ',', accuracy_dt)
     cm = confusion_matrix(y_test, y_pred)
     precision = precision_score(y_test, y_pred, average='micro')
     recall = recall_score(y_test, y_pred, average='micro')
@@ -48,7 +63,10 @@ def Random_Forest(x_train, x_test, y_train, y_test):
     y_pred = rd_forest.predict(x_test)
     accuracy = accuracy_score(y_test, y_pred)
     accuracy_rf = round(accuracy*100, 2)
-    print(accuracy, accuracy_rf)
+    acc_rf = round(rd_forest.score(x_train, y_train)*100, 2)
+    results.loc[2, 'Score'] = acc_rf
+    results.loc[2, 'Accuracy_Score'] = accuracy_rf
+    #print("Random Forest: ", accuracy, ',', accuracy_rf)
     cm = confusion_matrix(y_test, y_pred)
     precision = precision_score(y_test, y_pred, average='micro')
     racall = recall_score(y_test, y_pred, average='micro')
@@ -60,7 +78,10 @@ def KNN(x_train, x_test, y_train, y_test):
     y_pred = knn.predict(x_test)
     accuracy = accuracy_score(y_test, y_pred)
     accuracy_knn = round(accuracy*100, 2)
-    print(accuracy, accuracy_knn)
+    acc_knn = round(knn.score(x_train, y_train)*100, 2)
+    results.loc[3, 'Score'] = acc_knn
+    results.loc[3, 'Accuracy_Score'] = accuracy_knn
+    #print("KNeighborsClassifier: ", accuracy, ',', accuracy_knn)
     cm = confusion_matrix(y_test, y_pred)
     precision = precision_score(y_test, y_pred, average='micro')
     recall = recall_score(y_test, y_pred, average='micro')
@@ -72,12 +93,35 @@ def MLP(x_train, x_test, y_train, y_test):
     y_pred = mlp.predict(x_test)
     accuracy = accuracy_score(y_test, y_pred)
     accuracy_mlp = round(accuracy*100, 2)
-    print(accuracy, accuracy_mlp)
+    acc_mlp = round(mlp.score(x_train, y_train)*100, 2)
+    results.loc[4, 'Score'] = acc_mlp
+    results.loc[4, 'Accuracy_Score'] = accuracy_mlp
+    #print("Multi-Layer Perceptron: ", accuracy, ',', accuracy_mlp)
     cm = confusion_matrix(y_test, y_pred)
     precision = precision_score(y_test, y_pred, average='micro')
     recall = recall_score(y_test, y_pred, average='micro')
     f1 = f1_score(y_test, y_pred, average='micro')
 
+def SVC(x_train, x_test, y_train, y_test):
+    linear_svc = LinearSVC(max_iter=4000)
+    linear_svc.fit(x_train, y_train)
+    y_pred = linear_svc.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    accuracy_svc = round(accuracy*100, 2)
+    acc_svc = round(linear_svc.score(x_train, y_train)*100, 2)
+    results.loc[5, 'Score'] = acc_svc
+    results.loc[5, 'Accuracy_Score'] = accuracy_svc
+    #print("SVC: ", accuracy, ',', accuracy_svc)
+    cm = confusion_matrix(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='micro')
+    recall = recall_score(y_test, y_pred, average='micro')
+    f1 = f1_score(y_test, y_pred, average='micro')
+
+def assess():
+    #print(results.to_dict(orient="list"))
+    result_df = results.sort_values(by="Accuracy_Score", ascending=True)
+    result_df = results.reset_index(drop=True)
+    print(result_df)
 
 def proc(data):
     data.drop(columns="Id", inplace=True)
@@ -86,8 +130,12 @@ def proc(data):
     le = LabelEncoder()
     y = le.fit_transform(y)
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
-    #Naive_Bayes(x_train, x_test, y_train, y_test)
-    #Dec_Tree(x_train, x_test, y_train, y_test)
-    #Random_Forest(x_train, x_test, y_train, y_test)
-    #KNN(x_train, x_test, y_train, y_test)
-    #MLP(x_train, x_test, y_train, y_test)
+    #Some algorithms begin
+    Naive_Bayes(x_train, x_test, y_train, y_test)
+    Dec_Tree(x_train, x_test, y_train, y_test)
+    Random_Forest(x_train, x_test, y_train, y_test)
+    KNN(x_train, x_test, y_train, y_test)
+    MLP(x_train, x_test, y_train, y_test)
+    SVC(x_train, x_test, y_train, y_test)
+    #Some algorithms end
+    assess()
